e3a13e9a6d6e7f87d910cceeab7e543578586f69
Danish Ajmal <danishajmalbutt@gmail.com>
2025-02-20 23:24:57 +0500

    Update page.js

--- src/app/ProductCard/page.js
+++ src/app/ProductCard/page.js
@@ -40,7 +40,7 @@ export default function ProductCard() {
       <section>
         <div className="container mx-auto p-4">
           <h1 className="text-2xl font-bold mb-6 text-center">
-            Fan Categories
+            f
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {categories.map((category, index) => (
